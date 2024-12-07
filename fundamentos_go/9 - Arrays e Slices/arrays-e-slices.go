47a953b1d13b81a84a7748bf159ae933cc21b7b7
Luis Henrique <luis@prover.net.br>
2024-12-07 14:36:30 -0300

    Arrays e slices

---
+++ fundamentos_go/9 - Arrays e Slices/arrays-e-slices.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println("Arrays e Slices")
+
+	var array1 [5]string
+	array1[0] = "Posicao 1"
+	fmt.Println(array1)
+
+	array2 := [5]string{"Posicao 1", "Posicao 2", "Posicao 3", "Posicao 4", "Posicao 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	slice := []int{10, 11, 12, 13, 14, 15, 16, 17}
+	fmt.Println(slice)
+
+	slice = append(slice, 18)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+
+	array2[1] = "Posicao alterada"
+	fmt.Println(slice2)
+
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(array3))
+
+}
