d0184ba51d4555d4dff48ed6993c00937e2f729a
Luis Henrique <luis@prover.net.br>
2024-12-11 18:12:46 -0300

    Testes Automatizados

---
+++ fundamentos_go/18- Testes Automatizados/introducao/enderecos/enderecos_test.go
@@ -0,0 +1,33 @@
+package enderecos
+
+import "testing"
+
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
+func TestTipoDeEndereco(t *testing.T) {
+
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça das Rosas", "Tipo Inválido"},
+		{"Estrada Qualquer", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"AVENIDA REBOUÇAS", "Avenida"},
+		{"", "Tipo Inválido"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s",
+				tipoDeEnderecoRecebido,
+				cenario.retornoEsperado,
+			)
+		}
+	}
+
+}
